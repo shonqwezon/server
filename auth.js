a65d579113e07f9e82ea6d0c1decf65687d17f3e
Shon <62799691+shonqwezon@users.noreply.github.com>
2020-06-06 20:28:54 +0500

    Release vTest

--- auth.js
+++ auth.js
@@ -1,33 +1,32 @@
 var md5 = require('md5');
-var { Client } = require('pg');
+var pg = require('pg');
+var check = null;
 
-var client = new Client({
+var pool = new pg.Pool({
     user: 'qishon',
-    host: 'localhost',
+    host: '127.0.0.1',
     database: 'gpsdata',
     password: 'infiniti130191',
-    port: 5432
+    port: '5432'
 });
-
-var query = `INSERT INTO Authtentication (token, login, pass, email)VALUES ('04941350aefd81ffd355021e9411af50', 'john', 'doe', 'home@lol')`;
 
 module.exports.reg = function (data) {
     var token = md5(data.email + data.login + data.pass);
-    console.log(`Connection to gpsdata...`);
-    client.query(query, (err, res) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('Data insert successful');
-        client.end();
+    pool.query("INSERT INTO authentication(token, login, pass, email) VALUES('6fbhf1749124nfr', 'art', '123467123', 'home@bk.ru')", (err, result) => {
+        if(err) console.log(err);
+        pool.end();
     });
     return token;
 };
 
-/*
-module.exports.check = function (token) {
-    if ( == token) return true;
-    else return false;
+
+module.exports.check = function (info) {
+    pool.query(`SELECT token FROM authentication WHERE id = ${info.id};`, (err, result) => {
+        if (err) console.log(err);
+        if (result.rows[0].token == info.key) check = true;
+        else check = false;
+        pool.end();
+    });
+    return check;
 };
-*/
+
--- server.js
+++ server.js
@@ -2,24 +2,45 @@
 var express = require('express');
 var app = express();
 
-var { Client } = require('pg');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
-var client = new Client({
-    user: 'qishon',
-    host: 'localhost',
-    database: 'gpsdata',
-    password: 'infiniti130191',
-    port: 5432
+var auth = require('./auth');
+
+var location = null;
+
+app.get('/auth', urlencodedParser, (req, res) => {
+    res.sendFile(__dirname + '/auth.html');
+});
+
+app.post('/auth', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    return res.send(auth.reg(req.body));
 });
 
 
-app.post('/', function (req, res) {
-    console.log('req is comming');
-    client.connect();
-    console.log('Conecting to gpsdata...');
-    return res.sendStatus(200);
-    console.log('Connected...');
+
+app.get('/', (req, res) => {
+    res.send("Main page");
 });
+
+
+app.post('/location.send', jsonParser, (req, res) => {
+    if (auth.check(req.headers)) {
+        location = req.body;
+        return res.sendStatus(200);
+    }
+    else return res.sendStatus(401);
+
+});
+
+app.get('/location.get', (req, res) => {
+    if (auth.check(req.headers)) res.send(location);
+    else return res.sendStatus(401);
+});
+
 
 app.listen(8383);
 console.log('Server created on port 8383');
+    
